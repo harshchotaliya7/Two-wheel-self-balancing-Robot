6047a5d733a8585d049da6d400781ed57598f7f0
harshchotaliya7 <20bec039@nirmauni.ac.in>
2023-04-19 21:48:39 +0530

    working robot

--- TWSBR/TWSBR.ino
+++ TWSBR/TWSBR.ino
@@ -3,32 +3,38 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
+#define kp 1.5
+#define kd 150
 
 
-double Axis_angle=0;
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
+int DIR_PIN = 6;
+int PWM_PIN = 7;
+int DIR_PIN2 = 5;
+int PWM_PIN2 = 3;
+double axis_angle,diff_angle,prev_angle=0,prev_diff=0,derrivative;
+int speed=0;
 
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
 
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
+/*
+void loop() {
 
+
+  digitalWrite(DIR_PIN2, HIGH);
+
+  analogWrite(PWM_PIN2, 70);
+
+
+}
+*/
+
+
+/* 
    Connections
    ===========
    Connect SCL to SCL pin (analog 5 on Arduino UNO)
    Connect SDA to SDA pin (analog 4 on Arduino UNO)
    Connect VDD to 3-5V DC (depending on your board's logic level)
    Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-   2015/AUG/27  - Added calibration and system status helpers
 */
 
 /* Set the delay between fresh samples */
@@ -62,62 +68,6 @@ void displaySensorDetails(void)
 
 /**************************************************************************/
 /*
-    Display some basic info about the sensor status
-*/
-/**************************************************************************/
-void displaySensorStatus(void)
-{
-  /* Get the system status values (mostly for debugging purposes) */
-  uint8_t system_status, self_test_results, system_error;
-  system_status = self_test_results = system_error = 0;
-  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
-
-  /* Display the results in the Serial Monitor */
-  Serial.println("");
-  Serial.print("System Status: 0x");
-  Serial.println(system_status, HEX);
-  Serial.print("Self Test:     0x");
-  Serial.println(self_test_results, HEX);
-  Serial.print("System Error:  0x");
-  Serial.println(system_error, HEX);
-  Serial.println("");
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Display sensor calibration status
-*/
-/**************************************************************************/
-void displayCalStatus(void)
-{
-  /* Get the four calibration values (0..3) */
-  /* Any sensor data reporting 0 should be ignored, */
-  /* 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-
-  /* The data should be ignored until the system calibration is > 0 */
-  Serial.print("\t");
-  if (!system)
-  {
-    Serial.print("! ");
-  }
-
-  /* Display the individual values */
-  Serial.print("Sys:");
-  Serial.print(system, DEC);
-  Serial.print(" G:");
-  Serial.print(gyro, DEC);
-  Serial.print(" A:");
-  Serial.print(accel, DEC);
-  Serial.print(" M:");
-  Serial.print(mag, DEC);
-}
-
-/**************************************************************************/
-/*
     Arduino setup function (automatically called at startup)
 */
 /**************************************************************************/
@@ -137,17 +87,14 @@ void setup(void)
     while(1);
   }
 
-  delay(1000);
-
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(DIR_PIN2, OUTPUT);
+  pinMode(PWM_PIN2, OUTPUT);
+  digitalWrite(DIR_PIN,HIGH);
+  digitalWrite(DIR_PIN2,HIGH);
   /* Display some basic information on this sensor */
   displaySensorDetails();
-
-  /* Optional: Display current status */
-  displaySensorStatus();
-
-  bno.setExtCrystalUse(true);
-  
-
 }
 
 /**************************************************************************/
@@ -155,17 +102,87 @@ void setup(void)
     Arduino loop function, called once 'setup' is complete (your own code
     should go here)
 */
+
+void control_motor_speed(bool dir,int speed)
+{
+  switch(dir)
+  {
+    case 0:
+        digitalWrite(DIR_PIN, HIGH); 
+        analogWrite(PWM_PIN, speed); 
+        digitalWrite(DIR_PIN2, LOW); 
+        analogWrite(PWM_PIN2, speed); 
+        break;
+        
+    case 1:
+        
+        digitalWrite(DIR_PIN, LOW); 
+        analogWrite(PWM_PIN, speed); 
+        digitalWrite(DIR_PIN2, HIGH);
+        analogWrite(PWM_PIN2, speed);                     
+  }    
+}
+
+void control_motor_pid_with_speed(double ang,int max_speed)
+{
+  bool direction=0;
+ 
+  diff_angle=axis_angle-ang;
+  
+  derrivative=diff_angle-prev_diff;  
+  
+  speed = diff_angle*kp + derrivative*kd ;//+ ((diff_angle+10000)/fabs(diff_angle+10000)*60);
+  
+  prev_angle=axis_angle;
+  
+  prev_diff=diff_angle; 
+  
+  if(speed>max_speed)
+  {
+    speed=max_speed;
+  }     
+  else if (speed < 0-max_speed)
+  {
+    speed=-max_speed;          
+  }
+  if(speed>0)
+  {
+    direction=1;
+    speed=speed+60;
+  }
+  else
+  {
+    direction=0;
+    speed=speed-60;
+  }
+  //direction=~direction;
+  control_motor_speed(direction,fabs(speed));
+  
+}
+
 /**************************************************************************/
 void loop(void)
 {
-  /* Get a new sensor event */
+ 
   sensors_event_t event;
   bno.getEvent(&event);
+  axis_angle=event.orientation.z;
+  control_motor_pid_with_speed(-1,100);
 
-   
-  Serial.print("\tZ: ");
-  Serial.print(Axis_angle,4);
-  Serial.println("");
-  delay(BNO055_SAMPLERATE_DELAY_MS);
-  Axis_angle=event.orientation.z;
+
+  
+/**************************************************************************
+******************* PRINTING SENSOR DATA **********************************
+**************************************************************************/
+
+ // Serial.print("\tZ: ");
+ //Serial.println(speed);
+  
+  //control_motor_speed(1,30);
+  //analogWrite(R_PWM, 0);
+  //analogWrite(L_PWM, 50);  
+  //delay(BNO055_SAMPLERATE_DELAY_MS);
+  
+  
+
 }
